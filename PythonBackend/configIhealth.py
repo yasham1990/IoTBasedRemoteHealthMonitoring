3ae897cc8c8697fc623179c0b91dd8486d3ab3d7
yasham1990 <yasham1990@gmail.com>
2017-07-15 11:54:59 -0700

    Removed Credentials

--- PythonBackend/configIhealth.py
+++ PythonBackend/configIhealth.py
@@ -1,5 +1,5 @@
-CLIENT_ID = 'd48f3ac5755046bc9eb44e9db8f9523e'
-CLIENT_SECRET = 'fec444e2f7cc445e9c3fb46baba8dc6c'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
 CALLBACK_URI = 'http://localhost:3000/ihealth_auth_callback/'
 AUTH_URL = 'https://api.ihealthlabs.com:8443/OpenApiV2/OAuthv2/userauthorization/'
 USER_DATA_URL = 'https://api.ihealthlabs.com:8443/openapiv2/user/'
@@ -7,11 +7,11 @@ ALL_DATA_URL = 'https://api.ihealthlabs.com:8443/openapiv2/application'
 
 DATA_TYPES = {
     'OpenApiBP': {
-        'sc': 'a5b2cf2759254fd4a467d7e309b46368',
-        "sv": '3e5c93a4cacf4684887d012d62ad9a79'
+        'sc': '',
+        "sv": ''
     },
     'OpenApiUserInfo': {
-        'sc': 'a5b2cf2759254fd4a467d7e309b46368 ',
-        'sv': 'b336c96b2bc245f385488fb5401b1709'
+        'sc': '',
+        'sv': ''
     },
 }
